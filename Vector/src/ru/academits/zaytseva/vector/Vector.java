7d9c933dbd66608304cf90e6cd26146345451d38
Elza <ZaytsevaElza@yandex.ru>
2025-09-24 23:48:09 +0700

    innitial commit

--- Vector/src/ru/academits/zaytseva/vector/Vector.java
+++ Vector/src/ru/academits/zaytseva/vector/Vector.java
@@ -1,16 +1,180 @@
 package ru.academits.zaytseva.vector;
 
+import java.util.Arrays;
+
 public class Vector {
-    private int n;
     private double[] vector;
 
-    public Vector(int n, double[] vector) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Число n должно быть >= 0. Введено число n= " + n);
+    public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Число n должно быть > 0. Введено число n= " + n);
         }
-        this.n = n;
-        vector = new double[n];
+        this.vector = new double[n];
     }
 
+    public Vector(Vector otherVector) {
+        this.vector = Arrays.copyOf(otherVector.vector, otherVector.vector.length);
+    }
 
+    public Vector(double[] array) {
+        this.vector = Arrays.copyOf(array, array.length);
+    }
+
+    public Vector(int n, double[] array) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Число n должно быть > 0. Введено число n= " + n);
+        }
+        this.vector = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i < array.length) {
+                this.vector[i] = array[i];
+            } else {
+                this.vector[i] = 0;
+            }
+        }
+    }
+
+    public double[] getVector() {
+        return vector;
+    }
+
+    public void setVector(double[] vector) {
+        this.vector = vector;
+    }
+
+    public int getSize() {
+        return vector.length;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(vector).replace("[", "{").replace("]", "}");
+    }
+
+    public void addition(Vector secondVector) {
+        int n = vector.length;
+
+        if (n != secondVector.getSize()) {
+            throw new IllegalArgumentException("Размерность векторов должна быть одинаковая. " +
+                    "Размер первого вектора = " + n + ". " +
+                    "Размер второго вектора = " + secondVector.getSize());
+        }
+
+        for (int i = 0; i < n; i++) {
+            vector[i] += secondVector.vector[i];
+        }
+    }
+
+    public void difference(Vector secondVector) {
+        int n = vector.length;
+
+        if (n != secondVector.getSize()) {
+            throw new IllegalArgumentException("Размерность векторов должна быть одинаковая. " +
+                    "Размер первого вектора = " + n + ". Размер второго вектора = " + secondVector.getSize());
+        }
+
+        for (int i = 0; i < n; i++) {
+            vector[i] -= secondVector.vector[i];
+        }
+    }
+
+    public void multiScalar(double scalar) {
+        for (int i = 0; i < this.getSize(); i++) {
+            this.vector[i] *= scalar;
+        }
+    }
+
+    public void revers() {
+        for (int i = 0; i < this.getSize(); i++) {
+            this.vector[i] *= -1;
+        }
+    }
+
+    public double vectorLength() {
+        double squaresSum = 0;
+
+        for (double component : vector) {
+            squaresSum += component * component;
+        }
+
+        return Math.sqrt((squaresSum));
+    }
+
+    public double getComponent(int index) {
+        if (index < 0 || index > vector.length) {
+            throw new IllegalArgumentException("Индекс должен быть в диапазоне от 0 до " + (vector.length - 1));
+        }
+
+        return vector[index];
+    }
+
+    public void setComponent(int index, double value) {
+        if (index < 0 || index > vector.length) {
+            throw new IllegalArgumentException("Индекс должен быть в диапазоне от 0 до " + (vector.length - 1));
+        }
+
+        vector[index] = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+
+        Vector other = (Vector) obj;
+        return Arrays.equals(vector, other.vector);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+
+        return prime * hash + Arrays.hashCode(vector);
+    }
+
+    public static Vector getAddition(Vector vector1, Vector vector2) {
+        int vectorSize = Math.max(vector1.getSize(), vector2.getSize());
+
+        Vector result = new Vector(vectorSize);
+
+        for (int i = 0; i < vectorSize; i++) {
+            double value1 = (i < vector1.getSize()) ? vector1.getComponent(i) : 0;
+            double value2 = (i < vector2.getSize()) ? vector2.getComponent(i) : 0;
+            result.setComponent(i, value1 + value2);
+        }
+
+        return result;
+    }
+
+    public static Vector getDifference(Vector vector1, Vector vector2) {
+        int vectorSize = Math.max(vector1.getSize(), vector2.getSize());
+
+        Vector result = new Vector(vectorSize);
+
+        for (int i = 0; i < vectorSize; i++) {
+            double value1 = (i < vector1.getSize()) ? vector1.getComponent(i) : 0;
+            double value2 = (i < vector2.getSize()) ? vector2.getComponent(i) : 0;
+            result.setComponent(i, value1 - value2);
+        }
+
+        return result;
+    }
+
+    public static double getScalarProduct(Vector vector1, Vector vector2) {
+        int vectorSize = Math.min(vector1.getSize(), vector2.getSize());
+        double result = 0;
+
+        for (int i = 0; i < vectorSize; i++) {
+            result += vector1.getComponent(i)*vector2.getComponent(i);
+        }
+
+        return result;
+    }
 }
--- Vector/src/ru/academits/zaytseva/vector_main/Main.java
+++ Vector/src/ru/academits/zaytseva/vector_main/Main.java
@@ -1,4 +1,52 @@
 package ru.academits.zaytseva.vector_main;
 
+import ru.academits.zaytseva.vector.Vector;
+
 public class Main {
+    public static void main(String[] args) {
+        Vector V1 = new Vector(3);
+        Vector V2 = new Vector(new double[]{4.0, 7.5, 8.0});
+        Vector V3 = new Vector(3, new double[]{3.0, 6.5});
+        Vector V4 = new Vector(V2);
+        Vector V5 = new Vector(4, new double[]{1.2, 4.0, 3.7, 8.0});
+
+        System.out.println("Вектор V2 = " + V2);
+        System.out.println("Вектор V3 = " + V3);
+        System.out.println("Размер вектора V2 = " + V2.getSize());
+        System.out.println("Размер вектора V3 = " + V3.getSize());
+
+        V2.addition(V3);
+        System.out.println("Сумма векторов V2 + V3 = " + V2);
+
+        V2.difference(V3);
+        System.out.println("Разница между векторами V2 - V3 = " + V2);
+
+        V3.multiScalar(5);
+        System.out.println("Вектор V3 = " + V3);
+        System.out.println("Умножение вектора V4 на 5 = " + V3);
+
+        V3.revers();
+        System.out.println("Вектор V3 после разворота = " + V3);
+
+        double length = V5.vectorLength();
+        System.out.println("Вектор V5 = " + V5);
+        System.out.println("Длина вектора V5 = " + length);
+
+        double component = V5.getComponent(3);
+        System.out.println("Компонента вектора V5 с индексом 3 = " + component);
+
+        V5.setComponent(2, 5.6);
+        System.out.println("Вектор V5 = " + V5);
+
+        int hashV2 = V2.hashCode();
+        int hashV4 = V4.hashCode();
+
+        System.out.printf("Хэш-коды векторов V2, V4, V5 равны %d, %d%n", hashV2, hashV4);
+
+        Vector sum = Vector.getAddition(V2, V5);
+        System.out.println("Сумма векторов V2 + V5 = " + sum);
+
+        double scalarProduct = Vector.getScalarProduct(V2, V5);
+        System.out.println("Сумма векторов V2 + V5 = " + scalarProduct);
+    }
 }
